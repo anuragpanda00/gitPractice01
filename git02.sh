58d6c53519b4ba93446261e5a89a6735beaf06b8
developer01 <anuragpanda99@yahoo.com>
2022-06-28 13:38:18 +0530

    My git02 and git02 in rebaseBranch01 commit

---
+++ git02.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+echo "28th JUNE 2022"
+echo
+echo "BYE"
+echo
--- git03.sh
+++ git03.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 
 echo "Welcome to 3rd file --- to check rebase branch"
+echo
+echo"Hi folks"
+echo
